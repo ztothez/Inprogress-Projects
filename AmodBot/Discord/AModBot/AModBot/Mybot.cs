469b61266198a3cc3432af905f1728026858e070
ztothez <34552429+ztothez@users.noreply.github.com>
2020-03-25 04:16:30 +0200

    Add files via upload

---
+++ AmodBot/Discord/AModBot/AModBot/Mybot.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.Commands;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AModBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+
+        public MyBot()
+    }
+}
