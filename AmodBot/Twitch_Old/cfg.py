007a4c53a4d6862a9b5d90ed4ecf9c1f9101d6fa
ztothez <34552429+ztothez@users.noreply.github.com>
2020-09-30 19:28:00 +0300

    Update cfg.py

--- AmodBot/Twitch_Old/cfg.py
+++ AmodBot/Twitch_Old/cfg.py
@@ -2,7 +2,7 @@
 HOST = "irc.chat.twitch.tv"                     # the Twitch IRC server
 PORT = 6667                                     # always use port 6667!
 NICK = "AmodBot"                                # your Twitch username, lowercase
-PASS = "oauth:n6o080roj5ctfpve32e50s5knvfl6k"   # your Twitch OAuth token
+PASS = "oauth:"   # your Twitch OAuth token
 CHAN = "z_to_the_z"                            # the channel you want to join
 
 # cfg.py
